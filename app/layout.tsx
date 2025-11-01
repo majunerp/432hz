45ce3b045d12fe102f186a8382f99e49fa856437
majunerp <majunerperp@gmail.com>
2025-11-01 21:52:52 +0800

    Add Microsoft Clarity tracking code (tzbme7pzb7)

--- app/layout.tsx
+++ app/layout.tsx
@@ -96,6 +96,17 @@ export default function RootLayout({
           `}
         </Script>
 
+        {/* Microsoft Clarity */}
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "tzbme7pzb7");
+          `}
+        </Script>
+
         <Navigation />
         <main className="flex-grow">{children}</main>
         <Footer />
